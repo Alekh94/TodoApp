be2959d85dec886488de8ff7d8e258c0a8c63217
Alekh Singh <macbookair@macbooks-macbook-air.local>
2019-03-24 21:17:51 +0100

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Macbook Air on 2019-03-24.
+//  Copyright © 2019 Alekh Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
