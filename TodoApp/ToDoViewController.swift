8d40963b2953757f41c398d4ce2d73a875658d05
Alekh Singh <macbookair@macbooks-macbook-air.local>
2019-04-04 22:30:13 +0200

    Add new item functionality complete

--- TodoApp/ToDoViewController.swift
+++ TodoApp/ToDoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
     
-    let itemArray = ["Shoppa", "Uppgifter", "Gärningar"]
+    var itemArray = ["Shoppa", "Uppgifter", "Gärningar"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,9 +52,43 @@ class ToDoViewController: UITableViewController {
         
         // Så att det inte ser ut att en rad är markerad hela tiden
         tableView.deselectRow(at: indexPath, animated: true)
-        
-       
-        
+    
     }
+    
+    //MARK - ADD new items
+    //Vi skapade knappen med hjälp av UIBarButton i Main Storyboard.
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        //Skapar en variabel för att sedan kunna använda data från en closure
+        var textField = UITextField()
+        
+        //Skapar en pop up där det står Add new Todo med hjälp av UIalertController
+        let alert = UIAlertController(title: "Add New Todo", message: "", preferredStyle: .alert)
+        
+        //Skapar en actionknapp som heter Add Item med hjälp av UIAlertAction
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happen once the user click on the add item button on our UIAlert
+            
+            //Lägga till texten från closure nedan i itemArray som sedan ser till att synliggöra det
+            self.itemArray.append(textField.text!)
+            // Uppdaterar tabellenviewen och därmed synliggörs det man skrev in i raden ovan
+            self.tableView.reloadData()
+                }
+        
+        // En closure --> lägger till en textfield till alert (konstanten som innehåler pop up)
+        //placeholder gör att det finns en grå text som försvinner sedan när man trycker på fältet
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        
+        //Det här händer när man trycker på knappen
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
